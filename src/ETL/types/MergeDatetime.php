bfb38a9db4932ff591346520f8029263157aea8a
Joey de Ruiter <contact@joeyderuiter.me>
2018-01-15 19:10:53 +0100

    Files didnt go with the commit

---
+++ src/ETL/types/MergeDatetime.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace ETL\Types;
+
+class MergeDatetime implements IType, IMultiple {
+    private const SEPERATOR = '|';
+
+    private $row = null;
+    private $value      = '';
+    private $formatted  = '';
+    private $extras     = [];
+
+    public function setRow($row) {
+        $this->row = $row;
+    }
+
+    public function getRaw() {
+        return $this->value;
+    }
+
+    function extract($value) {
+        $this->value = $value;
+
+        foreach ($this->extras as $extra) {
+            $this->value .= self::SEPERATOR . $extra;
+        }
+    }
+
+    function transform() {
+        $this->formatted = $this->value;
+    }
+
+    function load() {
+        return $this->formatted;
+    }
+
+    function add(string $name, $value) {
+        $this->extras[$name] = $value;
+    }
+}
